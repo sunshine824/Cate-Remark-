30a6070fb7bca9cf86fdab073c6c013d006e28ce
sunshine824 <cx790168710@aliyun.com>
2017-06-26 16:23:40 +0800

    滚动加载更多

--- src/components/LoadMore/index.js
+++ src/components/LoadMore/index.js
@@ -8,13 +8,40 @@ export default class LoadMore extends Component {
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
     }
 
-    loadMoreHandle(){
+    loadMoreHandle() {
         this.props.loadMoreFn()
+    }
+
+    componentDidMount() {
+        const loadMoreFn = this.props.loadMoreFn
+        const wrapper = this.refs.wrapper
+
+        let timeoutId;
+
+        function callback() {
+            const top = wrapper.getBoundingClientRect().top //获取距离顶部距离
+            const windowHeight = window.screen.height //获取屏幕高度
+
+            if (top && top < windowHeight) {
+                //当wrapper 已经被滚动到暴露在页面的可视范围触发
+                loadMoreFn()
+            }
+        }
+
+        window.addEventListener('scroll', () => {
+            if (this.props.isLoadingMore) {
+                return
+            }
+            if (timeoutId) {
+                clearInterval(timeoutId)
+            }
+            timeoutId = setTimeout(callback, 50)
+        }, false)
     }
 
     render() {
         return (
-            <div className="load-more">
+            <div className="load-more" ref="wrapper">
                 {
                     this.props.isLoadingMore
                         ? <span>加载中...</span>
